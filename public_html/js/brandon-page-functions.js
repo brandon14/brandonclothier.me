6d77eb66cdff422467915070fe4758543431c09a
Brandon Clothier <brandon14125@gmail.com>
2016-08-20 22:19:57 -0400

    brandon.css: Animate the jumbotrons.
    brandon-page-functions: Modularize the page functions. Cache the DOM, do some
    initialization, then bind the events.

--- public_html/js/brandon-page-functions.js
+++ public_html/js/brandon-page-functions.js
@@ -1,45 +1,55 @@
-// Collapse navbar  on select
-$(document).on('click', '.navbar-collapse.in', function(e) {
-  if( $(e.target).is('a') && $(e.target).attr('class') !== 'navbar-toggle') {
-    $(this).collapse('hide');
-    if ($('.dark-overlay').is(':visible')) {
-      $('.dark-overlay').hide();
-    }
+var PageFunctions = (function() {
+  // Cache the DOM
+  var $darkOverlay = $('.dark-overlay');
+  var $navbarItems = $('#navbar-items > li > a');
+  var $navbarBrand = $('.navbar-brand');
+  var $offCanvasToggle = $('[data-toggle="offcanvas"]');
+  var $offCanvasRow = $('.row-offcanvas');
+  var $tabDataToggle = $('a[data-toggle="tab"]');
+  var $levelBar = $('.level-bar-inner');
+  var $navbarToggle = $('.navbar-toggle');
+  // End DOM caching
+
+  // Basic initialization
+  // Go to the latest tab, if it exists:
+  var lastTab = localStorage.getItem('lastTab');
+  if (lastTab) {
+    var tab = $('[href="' + lastTab + '"]').tab('show');
+    $navbarBrand.text($(tab).text());
   }
-});
 
-// Change the brand name on navbar item select
-$('#navbar-items > li > a').on('click', function() {
-  $('.navbar-brand').text($(this).text());
-});
+  // Set the progressbar widths to 0
+  $levelBar.css('width', '0');
+  // End init
 
-// Off canvas toggle
-$('[data-toggle="offcanvas"]').click(function () {
-  $('.row-offcanvas').toggleClass('active')
-});
-
-// Function to store the lat visited tab and select it upon page revisit
-$(function() { 
-    // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        // save the latest tab; use cookies if you like 'em better:
-        localStorage.setItem('lastTab', $(this).attr('href'));
-    });
-
-    // go to the latest tab, if it exists:
-    var lastTab = localStorage.getItem('lastTab');
-    if (lastTab) {
-        var tab = $('[href="' + lastTab + '"]').tab('show');
-        $('.navbar-brand').text($(tab).text());
+  // Bind events
+  // Collapse navbar on select
+  $(document).on('click', '.navbar-collapse.in', function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') !== 'navbar-toggle') {
+      $(this).collapse('hide');
+      if ($darkOverlay.is(':visible')) {
+        $darkOverlay.hide();
+      }
     }
-});
+  });
 
-// Function to populate and animate the skill progress bars
-$(function() {
-  $('.level-bar-inner').css('width', '0');
+  // Save the last selected tab to the local storage
+  $tabDataToggle.on('shown.bs.tab', function (e) {
+    localStorage.setItem('lastTab', $(this).attr('href'));
+  });
+
+  // Change the brand name on navbar item select
+  $navbarItems.on('click', function() {
+    $navbarBrand.text($(this).text());
+  });
+
+  // Off canvas toggle
+  $offCanvasToggle.on('click', function () {
+    $offCanvasRow.toggleClass('active')
+  });
 
   $(window).on('load', function() {
-    $('.level-bar-inner').each(function() {
+    $levelBar.each(function() {
       var itemWidth = $(this).data('level');
 
       $(this).animate({
@@ -47,9 +57,10 @@ $(function() {
       }, 800);
     });
   });
-});
 
-// Function to show the dark overlay whenever the navmenu is expanded
-$('.navbar-toggle').on('click', function() {
-  $('.dark-overlay').toggle();
-});
+  // Function to show the dark overlay whenever the navmenu is expanded
+  $navbarToggle.on('click', function() {
+    $darkOverlay.toggle();
+  });
+  // End event binding
+})();
