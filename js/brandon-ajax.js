ab4a81e532704ed9664dbf319bc67b5ada99a171
Brandon Clothier <brandon14125@gmail.com>
2016-09-03 23:42:33 -0400

    brandon-ajax.js: Simplfy the AJAX module.
    Add comments.

--- js/brandon-ajax.js
+++ js/brandon-ajax.js
@@ -1,7 +1,28 @@
+/**
+ * Module that handles the AJAX calls for this page. As of right now it only sends emails.
+ *
+ * @type    {object}
+ *
+ * @return  {object} sendEmail: Function to send an email
+ */
 var AjaxFunctions = (function() {
-  var ajaxUrl = '/ajax/send_email.php';
+  /**
+   * The url for the send-email AJAX script.
+   *
+   * @type  {string}
+   */
+  var ajaxUrl = '/ajax/send-email.php';
 
-  function _sendEmail(name, email, message, callback) {
+  /**
+   * Function to send an AJAX request to the send-email AJAX script to send a cotnact email
+   * to me.
+   *
+   * @param  {string}   name     The name of the person sending a contact email.
+   * @param  {string}   email    The email address of the person sending the contact email.
+   * @param  {string}   message  The contact email content.
+   * @param  {Function} callback Function to invoke upon AJAX request done.
+   */
+  function sendEmail(name, email, message, callback) {
     var request = $.ajax({
       type: 'POST',
       url: ajaxUrl,
@@ -25,16 +46,9 @@ var AjaxFunctions = (function() {
     });
   }
 
-  function sendEmail(name, email, message) {
-    var returnResponse;
-
-    _sendEmail(name, email, message, function(response) {
-      returnResponse = response;
-    });
-
-    return returnResponse;
-  }
-
+  /**
+   * Return our public API
+   */
   return {
     sendEmail : sendEmail
   }
