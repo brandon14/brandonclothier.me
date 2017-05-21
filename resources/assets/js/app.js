53842f98117771dcb00a030f770182406746d1fd
Brandon Clothier <brandon14125@gmail.com>
2017-05-21 04:01:57 -0400

    app.js: Clean up application JavaScript
    Move all logic inside closure.
    Use all import statements instead of require for Bootstrap.
    75px is the offset for Bootstrap affix.
    API will return errors in JSON with the key error and the message will be contained in the message key.
    Add variable JSDocs.
    Clean up grouping comments.
    
    Also in webpack.mix.js, use the ProvidePlugin to expose jQuery, $ and jquery to modules.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,64 +1,158 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 import $ from 'jquery';
 import axios from 'axios';
-
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// Set up CSRF token from meta tag for Axios AJAX requests
-const token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-  console.error('CSRF token not found.');
-}
-
-window.axios = axios;
-window.$ = $;
-window.jQuery = $;
-
-require('jquery.easing');
-require('bootstrap-sass');
-require('bootstrap-material-design');
+import 'jquery.easing';
+import 'bootstrap-sass';
+import 'bootstrap-material-design';
 
 (() => {
-  // ********** Contact API URL ********** //
+  /**
+   * Contact email API URL.
+   *
+   * @type {string}
+   */
   const ajaxUrl = '/contact';
 
-  // ********** DOM Caching ********** //
+  /**
+   * CSRF token retrieved from meta tag.
+   *
+   * @type {string}
+   */
+  const token = $('meta[name="csrf-token"]').attr('content');
+
+  /*
+   |--------------------------------------------------------------------------
+   | DOM Caching
+   |--------------------------------------------------------------------------
+   | Cache all of our jQuery DOM elements here for use later.
+   |
+   */
+
+  /**
+   * Window jQuery object.
+   *
+   * @type {jQuery object}
+   */
+  const $window = $(window);
+  /**
+   * Document jQuery object.
+   *
+   * @type {jQuery object}
+   */
+  const $document = $(document);
+  /**
+   * HTML jQuery object.
+   *
+   * @type {jQuery object}
+   */
+  const $html = $('html');
+  /**
+   * HTML and body jQuery object combined.
+   *
+   * @type {jQuery object}
+   */
+  const $htmlBody = $('html, body');
+  /**
+   * Navbar class jQuery object.
+   *
+   * @type {jQuery object}
+   */
+  const $navbar = $('.navbar');
+  /**
+   * Navbar toggle class jQuery object.
+   *
+   * @type {jQuery object}
+   */
+  const $navbarToggle = $('.navbar-toggle');
+  /**
+   * Dark overlay class div jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $darkOverlay = $('.dark-overlay');
 
-  const $window = $(window);
-  const $document = $(document);
-  const $htmlBody = $('html, body');
-  const $navbar = $('.navbar');
-  const $navbarToggle = $('.navbar-toggle');
-
+  /**
+   * Progress level bar class jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $levelBar = $('.level-bar-inner');
 
+  /**
+   * Contact form jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $contactForm = $('#contact-form');
+  /**
+   * Send email button jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $sendButton = $('#send-email');
+  /**
+   * Name input jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $name = $('#name');
+  /**
+   * Email input jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $email = $('#email');
+  /**
+   * Contact email message input jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $message = $('#email-message');
+  /**
+   * Contact email Bootstrap modal jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $modal = $('#contact-modal');
+  /**
+   * Modal content jQuery object.
+   *
+   * @type {jQuery object}
+   */
   const $modalContent = $('#modal-content');
 
-  const $html = $('html');
-  // ********** DOM Caching ********** //
+  /*
+   |--------------------------------------------------------------------------
+   | Basic Initialization
+   |--------------------------------------------------------------------------
+   | Perform some basic initialization for the page.
+   |
+   */
 
-  // ********** Basic initialization ********** //
+  // Initialize the material design theme
+  $.material.init();
+
+  // Set up axios headers
+  axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+  // Set up CSRF token from meta tag for Axios AJAX requests
+
+  if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  } else {
+    console.error('CSRF token not found.');
+  }
+
   // Set the progressbar widths to 0
   $levelBar.css('width', '0');
-  // ********** Basic initialization ********** //
 
-  // ********** Bind events ********** //
+  /*
+   |--------------------------------------------------------------------------
+   | Bind Events
+   |--------------------------------------------------------------------------
+   | Bind functions to the DOM element events.
+   |
+   */
+
   // Collapse navbar on select
   $document.on('click', '.navbar-collapse.in', hideOverlayOnSelect);
 
@@ -74,7 +168,7 @@ require('bootstrap-material-design');
   // Affix for navbar to collapse on scroll
   $navbar.affix({
     offset: {
-      top: 150,
+      top: 75,
     },
   });
 
@@ -83,9 +177,15 @@ require('bootstrap-material-design');
 
   // Send email on form submit
   $contactForm.on('submit', sendEmail);
-  // ********** Bind events ********** //
 
-  // ********** Function declarations ********** //
+  /*
+   |--------------------------------------------------------------------------
+   | Function Declarations
+   |--------------------------------------------------------------------------
+   | Declare application functions.
+   |
+   */
+
   /**
    * Function to reset the modal content.
    */
@@ -109,7 +209,7 @@ require('bootstrap-material-design');
   }
 
   /**
-   * Function to hide the dark-overlay and reenable scrolling on the page
+   * Function to hide the dark-overlay and re-enable scrolling on the page
    * whenever a navbar item is selected.
    *
    * @param {object} e Event.
@@ -135,7 +235,7 @@ require('bootstrap-material-design');
   /**
    * Function that will animate a single progress bar.
    */
-  function animateProgressBar(){
+  function animateProgressBar() {
     const itemWidth = $(this).data('level');
 
     $(this).animate({ width: itemWidth }, 800);
@@ -168,7 +268,7 @@ require('bootstrap-material-design');
   }
 
   /**
-   * Function to process the AJAX response and show a snackbar to inform the user the
+   * Function to process the AJAX response and show a modal to inform the user the
    * status of his contact email.
    *
    * @param {object} response JSON response from the AJAX call.
@@ -195,10 +295,10 @@ require('bootstrap-material-design');
 
   /**
    * Function to make the API request to send the contact email.
-   * @param {String}   name     Contact name.
-   * @param {String}   email    Contact from email address.
-   * @param {String}   message  Email message to send.
-   * @param {Function} callback Callback function to execute when response is returned.
+   * @param {string}   name     Contact name.
+   * @param {string}   email    Contact from email address.
+   * @param {string}   message  Email message to send.
+   * @param {function} callback Callback function to execute when response is returned.
    */
   function sendContactEmail(name, email, message, callback) {
     axios.post(ajaxUrl, {
@@ -216,8 +316,8 @@ require('bootstrap-material-design');
       if (error.response) {
         let errorMessage = 'Undefined error.';
 
-        if (error.response.data.error_message) {
-          errorMessage = error.response.data.error_message;
+        if (error.response.data.error.message) {
+          errorMessage = error.response.data.error.message;
         } else if (error.response.data.response) {
           errorMessage = error.response.data.response;
         } else {
@@ -243,7 +343,4 @@ require('bootstrap-material-design');
       callback(tResponse);
     });
   }
-  // ********** Function declarations ********** //
 })();
-
-$.material.init();
