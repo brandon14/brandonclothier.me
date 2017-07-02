6e592ff61a1f8b0a5a1a2647f7df5f8899025305
Brandon Clothier <brandon14125@gmail.com>
2017-07-01 23:56:49 -0400

    contact.blade: Fix contact email template.

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
 You have a contact email!<br />
-<strong>Name:</strong> {{ $name || 'Default' }}<br />
-<strong>Email:</strong> {{ $email || 'test@test.com' }}<br />
+<strong>Name:</strong> {{ $name ? $name : 'Default' }}<br />
+<strong>Email:</strong> {{ $email ? $email : 'test@test.com' }}<br />
 <strong>Date:</strong> {{ date('F jS, Y') }} at {{ date('h:i:s A T') }}<br />
 <br />
 <strong>Message:</strong>
 @component('mail::panel')
-{{ $message || 'No message' }}
+{{ $message ? $message : 'No message' }}
 @endcomponent
 
 Copyright {{ date('Y') }} {{ config('app.name') }}
