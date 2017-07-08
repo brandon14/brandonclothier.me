4b033ee54a46048c8d126f85b9bc658890a74afd
Brandon Clothier <brandon14125@gmail.com>
2017-07-07 22:08:47 -0400

    Minor fixes for Sensio Insight analysis.
    Also some potential bugfixes due to missing use statements.
    Update composer deps.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,10 +5,14 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
